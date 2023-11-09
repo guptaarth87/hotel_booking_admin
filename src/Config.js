3ee4a97d19e5f866475d7c8d04af6c8aef078673
guptaarth87 <guptaarth87@gmail.com>
2023-11-09 19:45:15 +0530

    final

--- src/Config.js
+++ src/Config.js
@@ -1,2 +1,2 @@
-// export const API_URL = "https://hotel-booking-vjlz.onrender.com/";
-export const API_URL = "http://localhost:1111/"
+export const API_URL = "https://hotel-booking-vjlz.onrender.com/";
+// export const API_URL = "http://localhost:1111/"
